18df0637f91dc24745f9215a43eb9a855668d20d
Hemanth Kumar <hemanth@alifsemi.com>
2025-06-04 13:27:26 +0530

    arch:arm:Update Copyright
    Update Copyright.
    
    Signed-off-by: Hemanth Kumar <hemanth@alifsemi.com>

--- arch/arm/mach-ensemble/ensemble.c
+++ arch/arm/mach-ensemble/ensemble.c
@@ -1,14 +1,8 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
-/* Copyright (C) 2024 Alif Semiconductor - All Rights Reserved.
- * Use, distribution and modification of this code is permitted under the
- * terms stated in the Alif Semiconductor Software License Agreement
- *
- * You should have received a copy of the Alif Semiconductor Software
- * License Agreement with this file. If not, please write to:
- * contact@alifsemi.com, or visit: https://alifsemi.com/license
- *
+// SPDX-License-Identifier: GPL-2.0-only
+/*
  * Define machines for Alif Ensemble series
  *
+ * Copyright (C) 2024 Alif Semiconductor
  * Author: Harith George <harith.g@alifsemi.com>
  */
 #include <linux/init.h>
@@ -29,7 +23,6 @@ static const char *const ensemble_dt_match[] __initconst = {
 };
 
 DT_MACHINE_START(ENSEMBLE_DT, "Alif Ensemble")
-	/* Maintainer: Harith George <harith.g@alifsemi.com> */
 	.dt_compat      = ensemble_dt_match,
 	.init_machine	= ensemble_init,
 MACHINE_END
