df82114c6f586c3d932fc8459932b6d047d8ef64
Akshay-AlifSemi <raghavendra.akshay@alifsemi.com>
2025-07-01 13:43:41 +0530

    drivers: iio: dac: Enable DAC12 reference voltage configuration via CMP0
    Allow configuration of the DAC12 reference voltage through the
    CMP0 register block.
    
    Signed-off-by: Akshay-AlifSemi <raghavendra.akshay@alifsemi.com>

--- drivers/iio/dac/dac-ensemble.c
+++ drivers/iio/dac/dac-ensemble.c
@@ -17,9 +17,8 @@
 #include <linux/platform_device.h>
 #include <linux/sizes.h>
 
-#define ANALOG_BASE		0x1A60A000
 #define	VBAT_ANA_REG2		0x3C
-
+#define CMP_COMP_REG2		0x004
 /* Ensemble DAC registers and bits */
 #define ENSEMBLE_DAC_REG1		0x000
 #define ENSEMBLE_DAC_IN			0x004
@@ -31,6 +30,7 @@
 #define DAC_MAX_INPUT            (0xFFFU)    /* Maximum input for the DAC is 4095
 					      * (DAC 12 bit resolution)
 					      */
+#define DEFAULT_INPUT_VALUE		0x0	/* Default Input Value in Input Register */
 #define DAC_IN_BYP_MUX           (1U << 1U)  /* Select the DAC input data source */
 #define DAC_MAX_BYP_VAL_Msk      (0x3FFCU)   /* DAC input data in bypass mode */
 #define DAC_TWOSCOMP_Pos          22U        /* Converts two's complement to unsigned binary data */
@@ -39,12 +39,21 @@
 #define DAC_IBIAS_VAL_Pos         23U        /* DAC buffer output current    */
 #define DAC_CAP_VAL_Pos           14U        /* DAC capacitance compensation */
 
-#define ANA_PERIPH_BG_ENA	(1 << 22)
+#define DAC12_VREF_CONT		(0x4 << 17)	/* Program DAC12_VREF_CONT for DAC12 */
+#define ADC_VREF_BUF_EN		(1 << 15)	/* Enable Vref ADC buffer enable */
+#define ADC_VREF_BUF_RDIV_EN	(0 << 16)	/* Enable Vref ADC divider enable */
+#define ANA_PERIPH_BG_ENA	(1 << 22)	/* Enable Analog Peripherals LDO */
 #define ANA_PERIPH_LDO_EN	(1 << 23)
+
+static void __iomem *dac_analog_base;
+static void __iomem *dac_cmp_base;
 
 struct ensemble_dac {
 	void __iomem *base;
+	void __iomem *analog_base;
+	void __iomem *cmp_base;
 	struct clk *clk;
+	struct clk *cmp_clk;
 };
 
 static const struct iio_chan_spec ensemble_dac_iio_channels[] = {
@@ -91,29 +100,56 @@ static int ensemble_dac_probe(struct platform_device *pdev)
 {
 	struct iio_dev *indio_dev;
 	struct ensemble_dac *dac;
-	void __iomem *analog_base;
-	struct resource *res;
 	int ret;
 	unsigned int reg;
 
 	indio_dev = devm_iio_device_alloc(&pdev->dev, sizeof(*dac));
 	if (!indio_dev)
 		return -ENOMEM;
-
 	platform_set_drvdata(pdev, indio_dev);
 	dac = iio_priv(indio_dev);
 
-	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	dac->base = devm_ioremap_resource(&pdev->dev, res);
-	if (IS_ERR(dac->base))
+	dac->base = devm_platform_ioremap_resource_byname(pdev, "dac");
+	if (IS_ERR(dac->base)) {
+		dev_err(&pdev->dev, "Failed to map DAC registers\n");
 		return PTR_ERR(dac->base);
+	}
+	if (!dac_analog_base) {
+		dac->analog_base = devm_platform_ioremap_resource_byname(pdev, "analog");
+		if (IS_ERR(dac->analog_base)) {
+			dev_err(&pdev->dev, "Failed to map Analog registers\n");
+			return PTR_ERR(dac->analog_base);
+		}
+		dac_analog_base = dac->analog_base;
+	} else {
+		dac->analog_base = dac_analog_base;
 
-	dac->clk = devm_clk_get(&pdev->dev, NULL);
+	}
+	if (!dac_cmp_base) {
+		dac->cmp_base = devm_platform_ioremap_resource_byname(pdev, "cmp");
+		if (IS_ERR(dac->cmp_base)) {
+			dev_err(&pdev->dev, "Failed to map CMP registers\n");
+			return PTR_ERR(dac->cmp_base);
+		}
+		dac_cmp_base = dac->cmp_base;
+	} else {
+		dac->cmp_base = dac_cmp_base;
+	}
+	dac->cmp_clk = devm_clk_get(&pdev->dev, "cmp_clk");
+	if (IS_ERR(dac->cmp_clk)) {
+		dev_err(&pdev->dev, "error getting clock\n");
+		return PTR_ERR(dac->cmp_clk);
+	}
+	ret = clk_prepare_enable(dac->cmp_clk);
+	if (ret) {
+		dev_err(&pdev->dev, "unable to enable clock\n");
+		goto exit;
+	}
+	dac->clk = devm_clk_get(&pdev->dev, "dac_clk");
 	if (IS_ERR(dac->clk)) {
 		dev_err(&pdev->dev, "error getting clock\n");
 		return PTR_ERR(dac->clk);
 	}
-
 	indio_dev->name = dev_name(&pdev->dev);
 	indio_dev->dev.parent = &pdev->dev;
 	indio_dev->info = &ensemble_dac_info;
@@ -126,14 +162,13 @@ static int ensemble_dac_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "unable to enable clock\n");
 		goto exit;
 	}
-
+	reg = readl(dac->cmp_base + CMP_COMP_REG2);
+	reg |= (DAC12_VREF_CONT | ADC_VREF_BUF_EN | ADC_VREF_BUF_RDIV_EN);
+	writel(reg, dac->cmp_base + CMP_COMP_REG2);
 	/* Enable Analog Peripheral */
-	analog_base = ioremap(ANALOG_BASE, SZ_64);
-	reg = readl(analog_base + VBAT_ANA_REG2);
+	reg = readl(dac->analog_base + VBAT_ANA_REG2);
 	reg |= (ANA_PERIPH_BG_ENA | ANA_PERIPH_LDO_EN);
-	writel(reg, analog_base + VBAT_ANA_REG2);
-	iounmap(analog_base);
-
+	writel(reg, dac->analog_base + VBAT_ANA_REG2);
 	reg = readl(dac->base + ENSEMBLE_DAC_REG1);
 	reg &= ~DAC_EN; /* DAC Disable */
 	writel(reg, dac->base + ENSEMBLE_DAC_REG1);
@@ -144,9 +179,7 @@ static int ensemble_dac_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "unable to register device\n");
 		goto dis_clk;
 	}
-
-	writel(0x0, dac->base + ENSEMBLE_DAC_IN);
-
+	writel(DEFAULT_INPUT_VALUE, dac->base + ENSEMBLE_DAC_IN);
 	return 0;
 
 dis_clk:
@@ -161,7 +194,6 @@ static void ensemble_dac_remove(struct platform_device *pdev)
 	struct ensemble_dac *dac = iio_priv(indio_dev);
 
 	iio_device_unregister(indio_dev);
-
 	/*TODO Disable DAC first */
 	clk_disable_unprepare(dac->clk);
 }
